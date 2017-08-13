a5045db247bafbaa1b58e4ae0acd65da9ecc24b1
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-13 17:32:13 +0200

    function index

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -1,2 +1,15 @@
 <?php
 
+namespace ShopSlim\Controllers;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Slim\Views\Twig;
+
+class HomeController
+{
+    public function index(Request $request, Response $response, Twig $view)
+    {
+        return $view->render($response, 'home.twig');
+    }
+}
