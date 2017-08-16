7d25cda1f73f2d3107279c33cf044cbc9cbc08c1
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-16 15:20:21 +0200

    connection with mysql

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,28 @@
 <?php
 
 use ShopSlim\App;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 session_start();
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = new App;
+
+$capsule = new Capsule;
+$capsule->addConnection([
+    'driver' => 'mysql',
+    'host' => '127.0.0.1',
+    'database' => 'ShopSlim',
+    'username' => 'root',
+    'password' => 'coderslab',
+    'charset' => 'utf8',
+    'collation' => 'utf8_unicode_ci',
+    'prefix' => ''
+]);
+
+$capsule->setAsGlobal();
+$capsule->bootEloquent();
 
 require __DIR__ . '/../app/routes.php';
 
