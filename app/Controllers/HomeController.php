48d268af6ca0bc0ea70e44c07c5a599fcc796c14
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-16 15:21:31 +0200

    product get

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -4,12 +4,18 @@ namespace ShopSlim\Controllers;
 
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
+use ShopSlim\Models\Product;
 use Slim\Views\Twig;
 
 class HomeController
 {
-    public function index(Request $request, Response $response, Twig $view)
+    public function index(Request $request, Response $response, Twig $view, Product $product)
     {
+        $products = $product->get();
+
+        var_dump($products->first()->title);
+        die();
+
         return $view->render($response, 'home.twig');
     }
 }
