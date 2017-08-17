cc0f5afb48abee184820a6dedfa371059b124f42
Rafał Ratajczyk <rafalratajczyk940@gmail.com>
2017-08-17 21:31:26 +0200

    route for product

--- app/routes.php
+++ app/routes.php
@@ -1,3 +1,4 @@
 <?php
 
 $app->get('/', ['ShopSlim\Controllers\HomeController', 'index'])->setName('home');
+$app->get('/products/{slug}', ['ShopSlim\Controllers\ProductController', 'get'])->setName('product.get');
